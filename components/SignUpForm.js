0d985e19a56f0b810da256063e380958d344d561
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-01-01 15:32:02 +0400

    async await try catch refactor done

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -7,14 +7,20 @@ class SignUpForm extends Component{
 
     state = {phone: ''} 
 
-    handleSubmit=()=>{
-     axios.post('https://us-central1-one-time-password-d5eab.cloudfunctions.net/create_user',{
+    handleSubmit = async() => {
+
+     try{
+     await axios.post('https://us-central1-one-time-password-d5eab.cloudfunctions.net/create_user',{
          phone: this.state.phone
-     }).then(()=>{
-         axios.post('https://us-central1-one-time-password-d5eab.cloudfunctions.net/request_one_time_password',{
-             phone: this.state.phone
-         })
      })
+         
+     await axios.post('https://us-central1-one-time-password-d5eab.cloudfunctions.net/request_one_time_password',{
+         phone: this.state.phone
+     })
+     } catch(err){
+         console.log(err)
+     }
+     
     }
 
     render(){
